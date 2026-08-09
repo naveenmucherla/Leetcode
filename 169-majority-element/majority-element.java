eba4755aedded6ca11eb0b07e0d0a4720f245583
Naveen <naveenmucharla583@gmail.com>
2026-08-09 08:38:57 +0530

    Time: 2 ms (70.91%) | Memory: 55.7 MB (32.38%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0 ;
+        int majority = 0 ;
+        for(int num : nums){
+            if(count == 0 ){
+                majority = num;
+            }
+            if(majority == num)
+              count++;
+            else
+              count--;
+        }
+
+        return majority;
+    }
+}
