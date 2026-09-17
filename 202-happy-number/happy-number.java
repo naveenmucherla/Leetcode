8f0807b3f639da9563ea6e6ac2087f77d003e0a9
Naveen <naveenmucharla583@gmail.com>
2026-09-17 20:50:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (94.57%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
-        
-        // Loop until we reach 1 or hit a cycle
-        while (n != 1 && !seen.contains(n)) {
-            seen.add(n);
-            n = getNext(n);
-        }
-        
-        return n == 1;
-    }
+        while (n != 1 && n != 4) {
+            int sum = 0;
 
-    // Helper method to calculate the sum of squares of digits
-    private int getNext(int num) {
-        int sum = 0;
-        while (num > 0) {
-            int digit = num % 10;
-            sum += digit * digit;
-            num /= 10; // Java integer division automatically truncates (like Math.floor)
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n / 10;
+            }
+
+            n = sum;
         }
-        return sum;
+
+        return n == 1;
+
         
    }
 }
