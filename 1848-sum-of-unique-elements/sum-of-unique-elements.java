51c1078f36435008e84b97f264a05b14aee79466
Naveen <naveenmucharla583@gmail.com>
2026-09-23 07:56:07 +0530

    Time: 2 ms (57.48%) | Memory: 43.3 MB (19.41%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -5,7 +5,7 @@ class Solution {
         for(int num :  nums){
            set.put(num , set.getOrDefault(num , 0) + 1);
         }
-        System.out.println(set);
+        //System.out.println(set);
         for(int num : set.keySet()){
             if(set.get(num) == 1){
                 sum += num;
